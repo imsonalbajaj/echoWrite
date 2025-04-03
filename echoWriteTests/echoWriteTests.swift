1334bcd9a9865351c1bc29e00312361fa0462323
Sonal <sonal@magicbricks.com>
2025-04-03 15:38:48 +0530

    Initial Commit

---
+++ echoWriteTests/echoWriteTests.swift
@@ -0,0 +1,17 @@
+//
+//  echoWriteTests.swift
+//  echoWriteTests
+//
+//  Created by Sonal on 03/04/25.
+//
+
+import Testing
+@testable import echoWrite
+
+struct echoWriteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
