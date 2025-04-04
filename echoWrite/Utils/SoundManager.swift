681c1e17fcceb9a2b7a4e1e214ab6f814f92444b
Sonal <sonal@magicbricks.com>
2025-04-04 12:15:29 +0530

    sound manager changes

--- echoWrite/Utils/SoundManager.swift
+++ echoWrite/Utils/SoundManager.swift
@@ -9,15 +9,19 @@ import Foundation
 import Speech
 import AVFoundation
 
+/* ENSURES ALL ACCESS IS SERIALIZED AND THREAD-SAFE */
 actor SoundManager {
     static let shared = SoundManager()
     
+    /* REQUESTS BOTH SPEECH RECOGNITION AND MICROPHONE PERMISSIONS CONCURRENTLY */
     func requestSpeechRecognitionPermission() async -> (Bool, Bool) {
         async let speechGranted = isSpeechRecognitionAuthorized()
         async let micGranted = isMicrophoneAccessGranted()
+        
         return await (speechGranted, micGranted)
     }
     
+    /* SPEECH RECOGNITION PERMISSION REQUEST CHECK */
     private func isSpeechRecognitionAuthorized() async -> Bool {
         await withCheckedContinuation { continuation in
             SFSpeechRecognizer.requestAuthorization { status in
@@ -26,6 +30,7 @@ actor SoundManager {
         }
     }
     
+    /* MICROPHONE ACCESS PERMISSION REQUEST CHECK */
     private func isMicrophoneAccessGranted() async -> Bool {
         await withCheckedContinuation { continuation in
             if #available(iOS 17.0, *) {
