92990418d5757f4a95a0d30ba2661bad84f9a563
sonal <imsonalbajaj@gmail.com>
2025-04-08 17:44:02 +0530

    fix

--- echowrite/echowriteApp.swift
+++ echowrite/echowriteApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct echowriteApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            SummaryItem.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
