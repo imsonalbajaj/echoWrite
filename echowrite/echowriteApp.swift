e7facbab7136dae312f04b25ab612150be263a29
sonal <imsonalbajaj@gmail.com>
2025-04-08 17:30:19 +0530

    initial message

---
+++ echowrite/echowriteApp.swift
@@ -0,0 +1,38 @@
+//
+//  echowriteApp.swift
+//  echowrite
+//
+//  Created by Sonal on 08/04/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct echowriteApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            Text("Hey There")
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
+/*
+ #Preview {
+     ContentView()
+         .modelContainer(for: Item.self, inMemory: true)
+ }
+ */
---
+++ echowriteTests/echowriteTests.swift
@@ -0,0 +1,17 @@
+//
+//  echowriteTests.swift
+//  echowriteTests
+//
+//  Created by Sonal on 08/04/25.
+//
+
+import Testing
+@testable import echowrite
+
+struct echowriteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
